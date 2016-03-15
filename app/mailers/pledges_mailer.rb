e4f377e91575d60d2fc53fab2f6d39d57e827489
Tammam Kbeili <tammam.kbeili@gmail.com>
2016-03-15 13:09:08 -0700

    add review for mailers

---
+++ app/mailers/pledges_mailer.rb
@@ -0,0 +1,9 @@
+class PledgesMailer < ApplicationMailer
+
+  def notify_campaign_owner(pledge)
+    @campaign = pledge.campaign
+    @owner    = @campaign.user
+    @pledge   = pledge
+    mail(to: @owner.email, subject: "Someone Pledges to Your Campaign!")
+  end
+end
